df06d78a75e5154103750c30916784dcd0bbfc5a
Brian Bixby <brianbixby0@gmail.com>
2018-03-07 08:36:53 -0800

    tests pass

--- lab-brian/route/list-router.js
+++ lab-brian/route/list-router.js
@@ -13,11 +13,16 @@ const listRouter = module.exports = Router();
 listRouter.post('/api/list', bearerAuth, jsonParser, function(req, res, next) {
   debug('POST: /api/list');
 
-
+  req.body.userID = req.user._id;
+  new List(req.body).save()
+    .then( list => res.json(list))
+    .catch(next);
 });
 
 listRouter.get('/api/list/:listId', bearerAuth, function(req, res, next) {
   debug('GET: /api/list/:listID');
 
-  
+  List.findById(req.params.listId)
+    .then( list => res.json(list))
+    .catch(next);
 });
