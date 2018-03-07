a2da0547fca79c7136a6b3d20833a4295444769d
Brian Bixby <brianbixby0@gmail.com>
2018-03-07 15:23:58 -0800

    all testing done except 3 puts

--- lab-brian/__test__/list-route.test.js
+++ lab-brian/__test__/list-route.test.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const request = require('superagent');
-const mongoose = require('mongoose');
 const serverToggle = require('../lib/server-toggle.js');
 const server = require('../server.js');
 
@@ -20,6 +19,15 @@ const exampleUser = {
 const exampleList = {
   name: 'test list',
   desc: 'test list desc',
+};
+
+const exampleList2 = {
+  name: 'test list2',
+  desc: 'test list desc2',
+};
+
+const exampleList3 = {
+  name: 'test list3',
 };
 
 describe('List routes', function() {
@@ -70,9 +78,151 @@ describe('List routes', function() {
           done();
         });
     });
+
+    it('should return a 401 error, no token', done => {
+      request.post(`${url}/api/list`)
+        .send(exampleList)
+        .set({
+          Authorization: 'Bearer ',
+        })
+        .end((err, res) => {
+          expect(res.status).toEqual(401);
+          done();
+        });
+    });
+
+    it('should return a 400 error, no body', done => {
+      request.post(`${url}/api/list`)
+        .send()
+        .set({
+          Authorization: `Bearer ${this.tempToken}`,
+        })
+        .end((err, res) => {
+          expect(res.status).toEqual(400);
+          done();
+        });
+    });
+
+    it('should return a 400 error, body error', done => {
+      request.post(`${url}/api/list`)
+        .send(exampleList3)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`,
+        })
+        .end((err, res) => {
+          expect(res.status).toEqual(400);
+          done();
+        });
+    });
   });
 
-  describe('GET: /api/list/:listId', () => {
+  describe('GET: /api/list/:listId && /api/list', () => {
+    beforeEach( done => {
+      new User(exampleUser)
+        .generatePasswordHash(exampleUser.password)
+        .then( user => {
+          this.tempUser = user;
+          return user.generateToken();
+        })
+        .then( token => {
+          this.tempToken = token;
+          done();
+        })
+        .catch(done);
+    });
+
+    beforeEach( done => {
+      exampleList.userID = this.tempUser._id.toString();
+      new List(exampleList).save()
+        .then( list => {
+          this.tempList = list;
+          done();
+        })
+        .catch(done);
+    });
+
+    beforeEach( done => {
+      exampleList2.userID = this.tempUser._id.toString();
+      new List(exampleList2).save()
+        .then( list2 => {
+          this.tempList2 = list2;
+          done();
+        })
+        .catch(done);
+    });
+
+    afterEach( () => {
+      delete exampleList.userID;
+    });
+
+    it('should return a list and a 200 status', done => {
+      request.get(`${url}/api/list/${this.tempList._id}`)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`,
+        })
+        .end((err, res) => {
+          if(err) return done(err);
+          expect(res.status).toEqual(200);
+          expect(res.body.name).toEqual(exampleList.name);
+          expect(res.body.desc).toEqual(exampleList.desc);
+          expect(res.body.userID).toEqual(this.tempUser._id.toString());
+          done();
+        });
+    });
+
+    it('should return a 401 when no token is provided', done => {
+      request.get(`${url}/api/list/${this.tempList._id}`)
+        .set({
+          Authorization: 'Bearer',
+        })
+        .end((err, res) => {
+          expect(res.status).toEqual(401);
+          done();
+        });
+    });
+
+    it('should return a 404 for a valid req with a list id not found', done => {
+      request.get(`${url}/api/list/a979e472c577c679758e018`)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`,
+        })
+        .end((err, res) => {
+          expect(res.status).toEqual(404);
+          done();
+        });
+    });
+
+    it('should return all lists and a 200 status', done => {
+      request.get(`${url}/api/lists`)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`, 
+        })
+        .end((err, res) => {
+          if(err) return done(err);
+          expect(res.status).toEqual(200);
+          expect(res.body[0].name).toEqual(exampleList.name);
+          expect(res.body[0].desc).toEqual(exampleList.desc);
+          expect(res.body[0].userID).toEqual(this.tempUser._id.toString());
+          expect(res.body[1].name).toEqual(exampleList2.name);
+          expect(res.body[1].desc).toEqual(exampleList2.desc);
+          expect(res.body[1].userID).toEqual(this.tempUser._id.toString());
+          done();
+        });
+    });
+
+    it('should return a 401 when no token is provided', done => {
+      request.get(`${url}/api/lists`)
+        .set({
+          Authorization: 'Bearer',
+        })
+        .end((err, res) => {
+          expect(res.status).toEqual(401);
+          done();
+        });
+    });
+  });
+
+  describe('DELETE: /api/list/:listId && /api/list', () => {
     beforeEach( done => {
       new User(exampleUser)
         .generatePasswordHash(exampleUser.password)
@@ -101,19 +251,97 @@ describe('List routes', function() {
       delete exampleList.userID;
     });
 
-    it('should return a list', done => {
-      request.get(`${url}/api/list/${this.tempList._id}`)
+    it('should delete a list and return a 204 status', done => {
+      request.delete(`${url}/api/list/${this.tempList._id}`)
         .set({
           Authorization: `Bearer ${this.tempToken}`,
         })
         .end((err, res) => {
-          expect(res.status).toEqual(200);
-          expect(res.body.name).toEqual(exampleList.name);
-          expect(res.body.desc).toEqual(exampleList.desc);
-          expect(res.body.userID).toEqual(this.tempUser._id.toString());
+          if(err) return done(err);
+          expect(res.status).toEqual(204);
+          done();
+        });
+    });
+
+    it('should not delete and return a 401 when no token is provided', done => {
+      request.delete(`${url}/api/list/${this.tempList._id}`)
+        .set({
+          Authorization: 'Bearer',
+        })
+        .end((err, res) => {
+          expect(res.status).toEqual(401);
+          done();
+        });
+    });
+
+    it('should not delete and return a 404 error for a valid req with a list id not found', done => {
+      request.delete(`${url}/api/list/404`)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`,
+        })
+        .end((err, res) => {
+          expect(res.status).toEqual(404);
+          done();
+        });
+    });
+
+    it('should not delete and return a 400 error for a req with no list id', done => {
+      request.delete(`${url}/api/list`)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`,
+        })
+        .end((err, res) => {
+          expect(res.status).toEqual(400);
           done();
         });
     });
   });
 
+  describe('PUT: /api/list/:listId && /api/list', () => {
+    beforeEach( done => {
+      new User(exampleUser)
+        .generatePasswordHash(exampleUser.password)
+        .then( user => {
+          this.tempUser = user;
+          return user.generateToken();
+        })
+        .then( token => {
+          this.tempToken = token;
+          done();
+        })
+        .catch(done);
+    });
+
+    beforeEach( done => {
+      exampleList.userID = this.tempUser._id.toString();
+      new List(exampleList).save()
+        .then( list => {
+          this.tempList = list;
+          done();
+        })
+        .catch(done);
+    });
+
+    afterEach( () => {
+      delete exampleList.userID;
+    });
+
+    it('should update and return a list with a 200 status', done => {
+      let updatedList = { name: 'test list2', desc: 'test list desc2' };
+      request.put(`${url}/api/list/${this.tempList._id}`)
+        .send(updatedList)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`,
+        })
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.status).toEqual(200);
+          expect(res.body.desc).toEqual(updatedList.desc);
+          expect(res.body.name).toEqual(updatedList.name);
+          expect(res.body.userID).toEqual(this.tempUser._id.toString());
+          done();
+        });
+    });
+
+  });
 });
--- lab-brian/route/list-router.js
+++ lab-brian/route/list-router.js
@@ -2,8 +2,8 @@
 
 const Router = require('express').Router;
 const jsonParser = require('body-parser').json();
-const createError = require('http-errors');
 const debug = require('debug')('16-basic-auth:list-router');
+const createError = require('http-errors');
 
 const List = require('../model/list.js');
 const bearerAuth = require('../lib/bearer-auth-middleware.js');
@@ -20,9 +20,44 @@ listRouter.post('/api/list', bearerAuth, jsonParser, function(req, res, next) {
 });
 
 listRouter.get('/api/list/:listId', bearerAuth, function(req, res, next) {
-  debug('GET: /api/list/:listID');
+  debug('GET: /api/list/:listId');
 
   List.findById(req.params.listId)
     .then( list => res.json(list))
     .catch(next);
+});
+
+listRouter.get('/api/lists', bearerAuth, function(req, res, next) {
+  debug('GET: /api/lists');
+
+  List.find()
+    .then(lists => res.json(lists))
+    .catch(next);
+});
+
+listRouter.put('/api/list/:listId', bearerAuth, jsonParser, function(req, res, next) {
+  debug('PUT: /api/list:listId');
+
+  List.findByIdAndUpdate(req.params.listId, req.body, {new: true})
+    .then( list => res.json(list))
+    .catch(next);
+});
+
+listRouter.put('/api/list', bearerAuth, jsonParser, function(req, res, next) {
+  debug('PUT: /api/list');
+  return next(createError(400, 'expected a list ID'));
+});
+
+
+listRouter.delete('/api/list/:listId', bearerAuth, function(req, res, next) {
+  debug('DELETE: /api/list/:listId');
+
+  List.findByIdAndRemove(req.params.listId)
+    .then( () => res.status(204).send())
+    .catch(next);
+});
+
+listRouter.delete('/api/list', bearerAuth, function(req, res, next) {
+  debug('DELETE: /api/list');
+  return next(createError(400, 'expected a list ID'));
 });
